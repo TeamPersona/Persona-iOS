6b628970ddf5ab60709b71ae3b3ac3800a1b1899
Denny Kim <dennykim93@gmail.com>
2015-07-29 22:48:53 -0400

    Change Dashboard tab to Messages tab

--- Persona/Tab View Controllers/Messages/MessagesViewController.m
+++ Persona/Tab View Controllers/Messages/MessagesViewController.m
@@ -1,25 +1,26 @@
 //
-//  DashboardViewController.m
+//  MessagesViewController.m
 //  Persona
 //
 //  Created by Denny Kim on 2015-06-23.
 //  Copyright (c) 2015 Persona. All rights reserved.
 //
 
-#import "DashboardViewController.h"
+#import "MessagesViewController.h"
 
-@interface DashboardViewController ()
+@interface MessagesViewController ()
 
 @end
 
-@implementation DashboardViewController
+@implementation MessagesViewController
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 }
 
-- (void)didReceiveMemoryWarning {
+- (void)didReceiveMemoryWarning
+{
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
